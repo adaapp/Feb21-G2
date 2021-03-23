20b8c554e2554dae08b6feb009a2cc8753e7bba3
Ethan Moss <ethan.moss@sykescottages.co.uk>
2021-03-23 14:58:29 +0000

    adding artresources

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express()
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.sendFile('./Public/index.html', { root: __dirname })
+})
+
+app.get('/artresources', (req, res) => {
+    res.sendFile('./Public/artresources.html', { root: __dirname })
 })
 
 app.listen(3000, () => {
